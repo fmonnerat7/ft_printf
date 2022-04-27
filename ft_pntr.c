8ffb44816dc995552a92371081e1cde60369fdb4
Felipe Monnerat <92029322+fmonnerat7@users.noreply.github.com>
2022-04-27 12:21:12 +0100

    Create ft_pntr.c

---
+++ ft_pntr.c
@@ -0,0 +1,20 @@
+static int	conversor(unsigned long long n, char *base, unsigned long long len)
+{
+	int	aux;
+
+	aux = 0;
+	if (n > (len - 1))
+		aux += conversor((n / len), base, len);
+	write(1, &base[(n % len)], 1);
+	return (aux + 1);
+}
+
+int	ft_pntr(unsigned long long n)
+{
+	int	aux;
+
+	aux = 0;
+	aux += write(1, "0x", 2);
+	aux += conversor(n, "0123456789abcdef", 16);
+	return (aux);
+}
